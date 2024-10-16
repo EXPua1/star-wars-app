3b57e5dfb666c8e4c61ffe117f0521759b6870c7
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2024-10-16 19:01:42 +0300

    changed path for image

--- src/components/CharactersList/CharactersList.jsx
+++ src/components/CharactersList/CharactersList.jsx
@@ -8,12 +8,8 @@ const CharactersList = ({ characters }) => {
         <li className={css.item} key={character.id}>
           <div>
             <img
-              src={`public/images/characters/${character.id}.jpg`}
+              src={`/images/characters/${character.id}.jpg`}
               alt={character.name}
-              onError={(e) => {
-                e.target.onerror = null;
-                e.target.src = "https://via.placeholder.com/300x400";
-              }} // Плейсхолдер
             />
             <p className={css.description}>{character.name}</p>
           </div>
